ecb200efcad6273cf507d82c6ea2e925a600b395
rambu <gill.222navdeep@gmail.com>
2018-02-16 00:50:04 +0530

    error_still-not_fixed

--- client/controller/books.js
+++ client/controller/books.js
@@ -1,13 +1,12 @@
 var myApp=angular.module('myApp');
-
-myApp.controller('BooksController',['$scope','$http','$location','$routeParams',function(){
+myApp.controller('BooksController',['$scope','$http','$location','$routeParams' ,function($scope,$http,$location,$routeParams){
 console.log('BooksController loaded.....');
 $scope.getBooks=function(){
 
-  $http.get('/api/book').success(function(response){
+  $http.get('/api/books').success(function(response){
 $scope.books=response;
 
   });
-}
+};
 
 }]);
